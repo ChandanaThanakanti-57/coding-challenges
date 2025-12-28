fd4e1cc519bfae3d2931c579e337ae1efabc9318
ChandanaThanakanti-57 <chanduthanakanti123@gmail.com>
2025-12-28 11:36:04 +0530

    Add files via upload

---
+++ combinations.py
@@ -0,0 +1,10 @@
+def combinations(n,k,curr,start):
+    if len(curr)==k:
+        print(curr)
+        return
+    for j in range(start,n+1):
+        curr.append(j)
+        combinations(n,k,curr,start)
+        curr.pop()
+combinations(4,2,[],1)
+        
